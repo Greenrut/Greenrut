84db6ed8201781facb1d08904526f7935fd5167a
Wealthometer <wealthhajoh87@gmail.com>
2026-07-11 04:58:31 +0100

    Update categoryController.js

--- server/src/controllers/categoryController.js
+++ server/src/controllers/categoryController.js
@@ -5,4 +5,5 @@ export async function listCategories(_req, res, next) {
     const results = await Product.aggregate([
       { $group: { _id: '$category', count: { $sum: 1 } } },
       { $sort: { _id: 1 } },
+    ])
 }
