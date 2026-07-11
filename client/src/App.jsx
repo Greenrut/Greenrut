d5c3e91da7195e6c023a2dd3ab585d417ccdcfaa
Wealthometer <wealthhajoh87@gmail.com>
2026-07-11 04:47:01 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -121,6 +121,7 @@ function App() {
   if (pathname === '/admin/tags') page = <AdminTagsPage pathname={pathname} onNavigate={navigate} />
   if (pathname === '/admin/settings') page = <AdminSettingsPage pathname={pathname} onNavigate={navigate} />
   if (
+    pathname.startsWith('/admin/') &&
     !pathname.startsWith('/admin/login') &&
     page = <AdminDashboardPage pathname={pathname} onNavigate={navigate} />
   }
