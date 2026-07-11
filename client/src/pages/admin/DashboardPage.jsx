0537acc280bb3b8ac4b4cdace77651ebca6634e8
Wealthometer <wealthhajoh87@gmail.com>
2026-07-11 05:07:16 +0100

    Update DashboardPage.jsx

--- client/src/pages/admin/DashboardPage.jsx
+++ client/src/pages/admin/DashboardPage.jsx
@@ -35,7 +35,6 @@ export function AdminDashboardPage({ pathname, onNavigate }) {
     loadDashboard()
   }, [])
 
-        setProducts(response.products || [])
         setRecentPosts(response.recentPosts || [])
       } catch (requestError) {
         if (!cancelled) {
