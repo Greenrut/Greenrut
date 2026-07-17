34e11bcca582c0fa3044f9e1da9bd6e73d6fa650
Alex-mfoniso <alexandermfoniso18@gmail.com>
2026-07-17 12:08:47 +0100

    kk

--- server/src/app.js
+++ server/src/app.js
@@ -31,7 +31,7 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions))
-app.options('*', cors(corsOptions))
+app.options(/.*/, cors(corsOptions))
 app.use(express.json({ limit: '2mb' }))
 app.use('/uploads', express.static(uploadsDir))
 
